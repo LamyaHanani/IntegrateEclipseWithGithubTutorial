6bcfa437236183de7ff11096ae1657f2190a9af1
Hp <Hp@192.168.1.3>
2019-02-04 21:21:32 +0200

    hiiii

---
+++ src/welcomproject/welcomproject.java
@@ -0,0 +1,11 @@
+package welcomproject;
+
+public class welcomproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+
+	}
+
+}
